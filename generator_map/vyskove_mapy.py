4ad4ee9d0d88de53a8528ef69ae45f0c142fdd11
Bezdek <stepan.bezdek@email.cz>
2025-01-27 15:13:15 +0100

    Prototyp interpolované výškové mapy -- vytvoří menší mřížku náhodných hodnot, převede na seznamy na víšku a na šířku, doplní na počet velké mřížky zinterpoluje a převede na mřížku

--- generator_map/vyskove_mapy.py
+++ generator_map/vyskove_mapy.py
@@ -9,10 +9,43 @@ def nahodne_pole(rows, cols, min_value=0, max_value=1):
     """
     return np.random.uniform(low=min_value, high=max_value, size=(rows, cols))
 
-rows = 5
-cols = 5
+
+def interpolovane_pole(big_rows, big_cols, small_rows, small_cols, min_value=0, max_value=1):
+    """
+    Vytvoří menší náhodnou mřížku, aplikuje ji na větší mřížku, chybějící hodnoty interpoluje.
+
+    """
+    # Vytvoření menší mřížky
+    small_grid = nahodne_pole(small_rows, small_cols, min_value, max_value)
+
+    # Indexy pro malou a velkou mřížku
+    small_x = np.linspace(0, big_cols - 1, small_cols)
+    small_y = np.linspace(0, big_rows - 1, small_rows)
+    big_x = np.arange(big_cols)
+    big_y = np.arange(big_rows)
+
+    # Vytvoření seznamu souřadnic
+    small_points = np.array([(x, y) for y in small_y for x in small_x])
+    small_values = small_grid.flatten()
+    big_points = np.array([(x, y) for y in big_y for x in big_x])
+
+    # Doplnění hodnot seznamu interpolací
+    big_list = griddata(small_points, small_values, big_points, method='cubic')
+
+    # Převedení na mřížku
+
+    big_grid = big_list.reshape(big_rows, big_cols)
+
+    return big_grid
+
+
+# Příklad použití
+big_rows = 9
+big_cols = 9
+small_rows = 3
+small_cols = 3
 min_value = 0
 max_value = 1
 
-random_array = nahodne_pole(rows, cols, min_value, max_value)
-print(random_array)
+interpolated_grid = interpolovane_pole(big_rows, big_cols, small_rows, small_cols, min_value, max_value)
+print(interpolated_grid)
